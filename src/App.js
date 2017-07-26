436044ab6ff724f485ea56c204d9fe9eb7a0b3c5
Ben Lu <ben@ben-l.com>
2017-07-27 01:23:22 +1200

    Added script copy

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
+import {init, animate} from './utils'
 import './App.css'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className="App" id='container'>
         TODO
       </div>
     )
